5ca567e6136e7f9d8ca0e4f464a53cc320149e6e
Graham Poulter <graham.poulter@gmail.com>
2006-10-26 12:46:45 -0200

    Fixed db2pmids

--- utils/db2pmids.py
+++ utils/db2pmids.py
@@ -12,7 +12,7 @@ Iterate through <dbfile>, and print all the PubMed IDs therein to
 from medline import FeatureDatabase
 import sys
 
-d = FeatureDatabase(sys.argv, 'r')
+d = FeatureDatabase(sys.argv[1], 'r')
 f = file(sys.argv[1], "w")
 for key in d:
     f.write("%d\n" % key)

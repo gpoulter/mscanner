193616faf512d29dd0e02d240cdf3986ed311fa6
Graham Poulter <graham.poulter@gmail.com>
2007-07-12 11:58:15 -0200

    Fixes to envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 host=$(hostname)
+echo "Host is $host"
 
 if [ "$host" == "jonbn" ]; then
 
@@ -9,16 +10,16 @@ if [ "$host" == "jonbn" ]; then
   export PYTHONPATH='C:\Documents and Settings\Graham\My Documents\data\mscanner\source'
   munge before PATH "$py25"
 
-elif [ "$host" == "eldar" ]; then
+elif [ "${host:0:5}" == "eldar" ]; then
 
   py25=/media/eldar/Python25
   export CHEETAH="$py25/python $(cygpath -m $py25/Scripts/cheetah)"
-  export PYTHONPATH='C:\Documents and Settings\Graham\My Documents\data\mscanner\source'
+  export PYTHONPATH='C:\Documents and Settings\Graham\My Documents\mirror\jonbn\mscanner\source'
   munge before PATH "$py25"
 
 elif [ "$host" == "maples" ]; then
 
   export CHEETAH=cheetah
-  export PYTHONPATH="$HOME/source/mscanner"
+  export PYTHONPATH="$HOME/source"
 
 fi

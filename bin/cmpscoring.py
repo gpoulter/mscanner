a4bfb4df51edd7d82376ff2c4caa881720c94010
Graham Poulter <graham.poulter@gmail.com>
2007-10-19 14:03:58 -0200

    Added cmpscores utility

---
+++ bin/cmpscoring.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""Tests different scoring methods to see whether there is a
+substantial difference in performance"""
+
+                                     
+__author__ = "Graham Poulter"                                        
+__license__ = "GPL"
+
+import sys
+
+from mscanner.configuration import rc, start_logger
+from mscanner.medline.Databases import Databases
+from mscanner.ValidationManager import ValidationManager
+from mscanner import utils
+
+
+score_methods = [
+    "scores_noadjust",
+    "scores_withabsence",
+    "scores_newpseudo",
+    "scores_oldpseudo",
+    "scores_rubin" ]
+
+
+def do_comparisons():
+    env = Databases()
+    #train_rel = rc.corpora / "pharmgkb-070205.txt"
+    #train_irrel = rc.corpora / "medline07-100k.txt"
+    train_rel = rc.corpora / "genedrug-small.txt"
+    train_irrel = rc.articlelist
+    for method in score_methods:
+        rc.dataset = "pg07-" + method
+        v = ValidationManager(rc.working / "cmpscores" / rc.dataset, env)
+        v.validation(train_rel, train_irrel)
+    env.close()
+    
+
+if __name__ == "__main__":
+    start_logger()
+    do_comparisons()
